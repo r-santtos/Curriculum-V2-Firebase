f7d77a666ee600b74ea2bc0e6eefe6f3e431bd54
r-santtos <rsanttos.dev@gmail.com>
2022-04-05 20:27:53 -0300

    finish app

---
+++ src/components/Splash/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import LottieView from 'lottie-react-native';
+import { View } from "react-native";
+import loadingAnimation from '../../animations/loading.json';
+
+const Lottie = () => {
+  return (
+    <View style={{
+      width: '100%', 
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#02080F'
+    }}>
+      <LottieView
+        style={{
+          height: 250,
+          alignSelf: 'center',
+        }}
+        autoPlay
+        loop
+        autoSize
+        resizeMode="cover"
+        source={loadingAnimation}
+      />
+    </View>
+  )
+}
+
+export default Lottie;
--- src/routes/StackRoutes/index.tsx
+++ src/routes/StackRoutes/index.tsx
@@ -1,22 +1,83 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import firestore from '@react-native-firebase/firestore';
+import * as SecureStore from 'expo-secure-store';
+
+import submit from "../../modules/save";
 
 /** IMPORT ROUTES */
 import Profile from '../../screens/Profile';
 import Form from '../../screens/Form';
+import Splash from '../../components/Splash';
+
+import { userListUser, userTokenProfile } from '../../context';
 
 export default function Routes() {
-  const { Navigator, Screen } = createNativeStackNavigator();
+  const { user, setUser } = userListUser();
+  const firebaseServer = () => {  
+    async function userProfileFirebase() {
+      let result = await SecureStore.getItemAsync('profile_email_cv');
+      if (result) {
+        const unsubscribe = firestore()
+        .collection('users')
+        .doc(result)
+        .onSnapshot(async (doc) => {
+          if (doc.exists) {
+            const data = doc.data();
+            setUser(data);
+          } else {
+            console.log('No such document!');
+          }
+          return () => unsubscribe();
+        });
+      }
+    }
+  
+    return {
+      userProfileFirebase,
+    }
+  }
 
+  /** CALL PROFILE FIREBASE CV */
+  const { tokenProfile } = userTokenProfile();
+  const server = firebaseServer();
+  useEffect(() => {server.userProfileFirebase()} ,[tokenProfile])
+
+  /** SAVE DATABASE FIREBASE */
+  const saveSubmit = submit();
+  const saveDatabaseApi = async () => {
+    let profile_email = await SecureStore.getItemAsync('profile_email_cv');
+    try {
+      if (!profile_email) {
+        saveSubmit.submit();
+        console.log(profile_email)
+      } else {
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {saveDatabaseApi()}, []);
+
+  const { Navigator, Screen } = createNativeStackNavigator();
   return (
     <View style={{backgroundColor: '#0d0d10', flex: 1}}>
       <Navigator>
-        <Screen
-          name="Profile"
-          component={Profile}
-          options={{headerShown: false}}
-        />
+        {user.email ? 
+          <Screen
+            name="Profile"
+            component={Profile}
+            options={{headerShown: false}}
+          />
+        :
+          <Screen
+            name="Splash"
+            component={Splash}
+            options={{headerShown: false}}
+          />
+        }
 
         <Screen
           name="Form"
